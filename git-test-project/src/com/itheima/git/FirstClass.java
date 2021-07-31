b5275711ba729defa456859a3cbec2d814e7ec86
yyz <2115528935@qq.com>
2021-07-31 11:41:48 +0800

    修改FirstClass

--- git-test-project/src/com/itheima/git/FirstClass.java
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -3,9 +3,11 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello git");
+        //hello test
     }
 
     public void test(){
         int i=10;
+        System.out.println(i);
     }
 }
